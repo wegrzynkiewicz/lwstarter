4b0206bb6a6b2a938c38c289b3ac8fced7b57369
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-10-21 22:12:17 +0200

    Development

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+qwe-docker-network-prune () {
+    docker network prune -f
+}
+
 qwe-docker-remove-all () {
+    docker ps --all --quiet | xargs docker rm --force
+}
+
+qwe-docker-remove-exited () {
     docker ps --all --filter="status=exited" --quiet | xargs docker rm --force
 }
 
--- aliases/math.sh
+++ aliases/math.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-qwe-hexadecimal-to-decimal () {
+qwe-bc-hexadecimal-to-decimal () {
    echo "obase=10; ibase=16; ${@:1}" | bc
 }
 
-qwe-hexadecimal-to-binary () {
+qwe-bc-hexadecimal-to-binary () {
    echo "obase=2; ibase=16; ${@:1}" | bc
 }
 
-qwe-binary-to-decimal () {
+qwe-bc-binary-to-decimal () {
    echo "obase=10; ibase=2; ${@:1}" | bc
 }
 
-qwe-binary-to-hexadecimal () {
+qwe-bc-binary-to-hexadecimal () {
    echo "obase=16; ibase=2; ${@:1}" | bc
 }
 
-qwe-decimal-to-hexadecimal () {
+qwe-bc-decimal-to-hexadecimal () {
    echo "obase=16; ibase=10; ${@:1}" | bc
 }
 
-qwe-decimal-to-binary () {
+qwe-bc-decimal-to-binary () {
    echo "obase=2; ibase=10; ${@:1}" | bc
 }
 
